ec2d47d57078ec23dec171e133c33c407389864a
Parth <parth1493@gmail.com>
2019-10-06 17:03:50 -0400

    simple Room data example with Asyc Task.     - removed CRUD logic from main thread.     - added Room callback.

--- app/src/main/java/com/parth/allfeatureandroidapp/db/MyAppDatabase.java
+++ app/src/main/java/com/parth/allfeatureandroidapp/db/MyAppDatabase.java
@@ -6,6 +6,7 @@ import androidx.room.RoomDatabase;
 import com.parth.allfeatureandroidapp.db.entity.Contact;
 
 @Database(entities = {Contact.class}, version = 1)
+
 public abstract class MyAppDatabase extends RoomDatabase {
 
     public abstract MyDao myDao();
